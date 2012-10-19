ede176fd7d52198f725a76bd28f4ecaaad1bfcd2
Matt McKegg <matt@wetsand.co.nz>
2012-10-19 16:47:55 +1300

    Added hooks for getting inner changes and added 'before sort' system

--- json-context.js
+++ json-context.js
@@ -4,7 +4,6 @@ var jsonChangeStream = require('json-change-stream')
   
   
 module.exports = function(data, options){
-  // options: dataFilters
   // options: dataFilters, matchers
   
   var options = options || {}
@@ -25,12 +24,7 @@ module.exports = function(data, options){
       } else if(changeInfo.action == 'append'){
         append(object, changeInfo)
       } else if (changeInfo.action == 'update'){
-        if (inCorrectCollection(object, changeInfo)){
-          update(object, changeInfo)
-        } else {
-          remove(changeInfo.original, changeInfo)
-          append(object, changeInfo)
-        }
+        update(object, changeInfo)
       }
     }
   )
@@ -69,16 +63,86 @@ module.exports = function(data, options){
   }
   
   function update(newObject, changeInfo){
+    
+    var removedItems = []
+    var addedItems = []
+    
+    function mergeInto(original, changed, preserveKeys){
+      preserveKeys = preserveKeys || []
+      var newKeys = Object.keys(changed)
+      var originalLength = Array.isArray(original) && original.length
+
+      newKeys.forEach(function(key){
+        // only update actual attributes - ignore meta attributes
+        if (!isMeta(key)){
+          if (original[key] !== changed[key]){
+            if ((original[key] instanceof Object) && (changed[key] instanceof Object)){
+              // recursive update
+              mergeInto(original[key], changed[key])
+            } else {
+              original[key] = changed[key]
+              
+              // for handling element sources that are strings/numbers rather than objects
+              if (original.$proxy && original.$proxy[key]){
+                original.$proxy[key].value = changed[key]
+              }
+              
+            }
+          }
+        }
+
+      })
+
+      if (Array.isArray(original)){
+        for (var i=changed.length;i<original.length;i++){
+          if (original.$proxy && original.$proxy[i]){
+            removedItems.push({collection: original, item: original.$proxy[i], index: i})
+          } else {
+            removedItems.push({collection: original, item: original[i], index: i})
+          }
+        }
+        for (var i=originalLength;i<changed.length;i++){
+          addedItems.push({collection: original, item: original[i], index: i})
+        }
+        
+        // remove unused keys
+        // truncate to length of new array
+        original.length = changed.length
+        if (original.$proxy){
+          original.$proxy.length = changed.length
+        }
+      } else {
+        Object.keys(original).filter(function(key){
+          return !isMeta(key) && (!~newKeys.indexOf(key)) && (!~preserveKeys.indexOf(key))
+        }).forEach(function(key){
+          delete original[key]
+        })
+      }
+    }
+    
     var query = context.query(changeInfo.matcher.item, newObject)
     if (query.value){
+      
             
       var object = query.value
         , collection = jsonQuery.lastParent(query)
       
-      var original = deepClone(object)
-      changeInfo = mergeClone(changeInfo, {action: 'update', collection: collection, original: original})
+      // sort item
       
-      mergeInto(object, newObject)
+      var original = deepClone(object)
+      changeInfo = mergeClone(changeInfo, {
+        action: 'update', 
+        collection: collection, 
+        original: original, 
+        removedItems: removedItems, 
+        addedItems: addedItems, 
+        key: query.key
+      })
+      
+      mergeInto(object, newObject, changeInfo.matcher.preserveKeys)
+      
+      changeInfo = checkCorrectCollection(object, changeInfo)      
+      changeInfo = checkSorting(object, changeInfo)
 
       // notify changes
       context.emit('change', object, changeInfo)
@@ -131,62 +195,102 @@ module.exports = function(data, options){
       }
     } else {
       
-      var collection = context.get(changeInfo.matcher.collection, newObject, {force: []})
-      changeInfo = mergeClone(changeInfo, {action: 'append', collection: collection, key: collection.length-1, filter: changeInfo.matcher.filter})
+      if (changeInfo.matcher.collection){
+        var collection = context.get(changeInfo.matcher.collection, newObject, {force: []})
+        changeInfo = mergeClone(changeInfo, {action: 'append', collection: collection, key: collection.length-1, filter: changeInfo.matcher.filter})
+
+        collection.push(newObject)
+      } else if (changeInfo.matcher.item){
+        
+        var query = context.query(changeInfo.matcher.item, newObject, {force: newObject})
+        var object = query.value
+          , collection = jsonQuery.lastParent(query)
+
+        changeInfo = mergeClone(changeInfo, {action: 'append', collection: collection, key: query.key, filter: changeInfo.matcher.filter})
+      }      
       
-      collection.push(newObject)
+      changeInfo = checkSorting(newObject, changeInfo)
+
       context.emit('change', newObject, changeInfo)
     }
   }
   
-  function inCorrectCollection(object, changeInfo){
+  function checkCorrectCollection(object, changeInfo){
+    //TODO: handle matcher.collectionKey
     if (changeInfo.matcher.collection){
-      var itemMatch = context.query(changeInfo.matcher.item, object)
-        , collectionMatch = context.get(changeInfo.matcher.collection, object, {force: []})
-        , currentCollection = jsonQuery.lastParent(itemMatch)
-      
-      if (itemMatch.value){
-        return collectionMatch === currentCollection
+    
+      var collectionMatch = context.get(changeInfo.matcher.collection, object, {force: []})      
+      if (Array.isArray(collectionMatch) && collectionMatch !== changeInfo.collection){
+        
+        // move to correct collection
+        changeInfo.collection.splice(changeInfo.key, 1)
+        collectionMatch.push(object)
+        
+        return mergeClone(changeInfo, {key: collectionMatch.length-1, collection: collectionMatch, originalCollection: changeInfo.collection})
       }
     }
-    return true
+    return changeInfo
+  }
+  
+  function checkSorting(object, changeInfo){
+    var collection = changeInfo.collection
+    var matcher = changeInfo.matcher
+
+    if (matcher.beforeSort && Array.isArray(collection)){
+      var beforeSorter = matcher.beforeSort
+      
+      var beforeKey = object[beforeSorter[0]]
+      if (beforeKey != null){
+        
+        // check to see if it is currently in correct location
+        var correctLocation = false
+        var index = changeInfo.key
+        if (beforeKey === -1){
+          if (index+1 === collection.length){
+            correctLocation = true
+          }
+        } else {
+          if (index < collection.length-1){
+            var currentBefore = collection[index+1]
+            if (currentBefore[beforeSorter[1]] === beforeKey){
+              correctLocation = true
+            }
+          }
+        }
+        
+        if (!correctLocation){
+          if (beforeKey === -1){
+            
+            // move to end
+            collection.splice(index, 1)
+            collection.push(object)
+            return mergeClone(changeInfo, {before: 'end', key: collection.length - 1})
+            
+          } else {                
+            
+            // or move to correct position
+            for (var i=0;i<collection.length;i++){
+              var beforeItem = collection[i]
+              if (beforeItem[beforeSorter[1]] === beforeKey){
+                if (i > index){
+                  i -= 1
+                }
+                collection.splice(index, 1)
+                collection.splice(i, 0, object)
+                return mergeClone(changeInfo, {before: beforeItem, key: i})
+              }
+            }
+            
+          }
+
+        }
+      }
+    }
+    return changeInfo
   }
  
   return context
 }
-
-function mergeInto(original, changed){
-
-  var newKeys = Object.keys(changed)
-
-  newKeys.forEach(function(key){
-  
-    // only update actual attributes - ignore meta attributes
-    if (!isMeta(key)){
-      if (original[key] !== changed[key]){
-        if ((original[key] instanceof Object) && (changed[key] instanceof Object)){
-          // recursive update
-          update(original[key], changed[key])
-        } else {
-          original[key] = changed[key]
-        }
-      }
-    }
-  
-  })
-  
-  // remove unused keys
-  if (Array.isArray(original)){
-    // truncate to length of new array
-    original.length = changed.length
-  } else {
-    Object.keys(original).filter(function(key){
-      return !isMeta(key) && (!~newKeys.indexOf(key))
-    }).forEach(function(key){
-      delete original[key]
-    })
-  }
-  
 module.exports.lastParent = jsonQuery.lastParent
 module.exports.obtain = function(object){
   return deepClone(object)
